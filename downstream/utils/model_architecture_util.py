fd09f0d2e1da7618bab74c237f8134a3f18a0563
Rui Yang <16921545+ruy204@users.noreply.github.com>
2022-07-18 15:59:50 -0400

    Add 5kb pretrained model

--- downstream/utils/model_architecture_util.py
+++ downstream/utils/model_architecture_util.py
@@ -294,3 +294,57 @@ class Disc(nn.Module):
         
         loss_val = F.binary_cross_entropy_with_logits(prediction, label, reduction='mean')
         return loss_val
+
+class Net_wider(nn.Module):
+    def __init__(self, num_layers=1, input_channels=30, window_size=12000):
+
+        super(Net_wider, self).__init__()
+        self.input_channels = input_channels
+        self.window_size = window_size
+
+        self.conv1 = ConvBlock(in_channels=self.input_channels, out_channels=70, kernel_width=17, stride=1, pool_size=4)
+        self.do1 = nn.Dropout(p = .1)
+        self.conv2 = ConvBlock(in_channels=70, out_channels=90, kernel_width=7, stride=1, pool_size=4)
+        self.do2 = nn.Dropout(p = .1)
+        self.conv3 = ConvBlock(in_channels=90, out_channels=70, kernel_width=5, stride=1, pool_size=4)
+        self.do3 = nn.Dropout(p = .1)
+        self.conv4 = ConvBlock(in_channels=70, out_channels=20, kernel_width=5, stride=1)
+        self.pool = nn.AdaptiveMaxPool1d(900//20)
+        self.do4 = nn.Dropout(p = .1)
+  
+        self.rnn1 = nn.LSTM(input_size=900, hidden_size=2400, num_layers=num_layers, batch_first=True, bidirectional=True)
+        self.rnn2 = nn.LSTM(input_size=4800, hidden_size=2400, num_layers=num_layers, batch_first=True, bidirectional=True)
+        # self.rnn3 = nn.LSTM(input_size=4800, hidden_size=1800, num_layers=num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(4800, 1200)
+        self.act = nn.ReLU()
+        self.fc2 = nn.Linear(1200, 200) #CHANGED 100 -> 200
+
+    def forward(self, x, hidden_state=None, seq_length=200):
+
+        assert x.shape[0] == self.input_channels
+
+        x = torch.as_strided(x, (seq_length, self.input_channels, self.window_size), (50, x.shape[1], 1))
+        x = self.conv1(x)
+        x = self.do1(x)
+        x = self.conv2(x)
+        x = self.do2(x)
+        x = self.conv3(x)
+        x = self.do3(x)
+        x = self.conv4(x)
+        x = self.pool(x)
+        x = self.do4(x)
+
+        x = x.view(1, seq_length, x.shape[1]*x.shape[2])  
+        res1, hidden_state = self.rnn1(x, None)
+        res2, hidden_state = self.rnn2(res1, None)
+        res2 = res2 + res1
+        # res3, hidden_state = self.rnn3(res2, None)
+        x = self.fc(res2) # + res3)
+        x = self.act(x)
+        x = self.fc2(x)
+        # x = self.act2(x)
+        return x, hidden_state
+
+    def loss(self, prediction, label, seq_length = 200, reduction='mean'):
+        l2_loss = F.mse_loss(prediction.view(-1, seq_length, 200), label.view(-1, seq_length, 200), reduction=reduction)
+        return l2_loss
